f6ae323d5a037f13856c1c31b62713c1763eb81d
umasspace <umas.space@gmail.com>
2026-03-06 23:34:12 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  experimental: {
+    // Enable experimental features if needed
+  },
+};
+
+export default nextConfig;
